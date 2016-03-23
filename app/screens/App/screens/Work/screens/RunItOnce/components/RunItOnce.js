0516e49672540667b0dbb82ba2df89d59203a8bc
Anthony Calzadilla <Anthony.Calzadilla@gmail.com>
2016-03-23 15:56:20 -0400

    fixed rotation vlues and created custom styles for deck offset

--- app/screens/App/screens/Work/screens/RunItOnce/components/RunItOnce.js
+++ app/screens/App/screens/Work/screens/RunItOnce/components/RunItOnce.js
@@ -38,9 +38,9 @@ class RunItOnce extends Component {
       }
 
       if (offset >= rotateCardsTop + 100) {
-         this.refs.rCard01.style.transform = "rotate(" + inc  + "deg)"
-         this.refs.rCard02.style.transform = "rotate(-" + inc  + "deg)"
-         this.refs.rCard03.style.transform = "rotate(" + inc  + "deg)"
+         this.refs.rCard01.style.transform = "rotate(-" + inc * 0.45 + "deg)"
+         this.refs.rCard02.style.transform = "rotate(" + inc * 0.45 + "deg)"
+         this.refs.rCard03.style.transform = "rotate(-" + inc * 0.45 + "deg)"
       }
 
       if (finalTop < 200) {
@@ -120,21 +120,22 @@ class RunItOnce extends Component {
 
 					<section className="case-study-deck" ref="deck">
 						<div className="container-sm">
-							<div className="grid-1-2">
+							<div className="deckCopy grid-copy">
+								<button className="button">Illustration</button> <button className="button">Photography</button>
+								<p>Run It Once, created by legendary poker player Phil Galfond, is a place for poker enthusiasts to gather and contribute professional-level strategy with others in the poker community. Besides the wealth of knowledge available at Run It Once, RIO’s brand is one classy act. With a clean, professional, and luxurious logo its no wonder their site is one of the best looking (and functioning) poker communities out there.</p>
+
+								<p>At Underbelly, we’re suckers for playing card designs. That’s one of the many reasons we were stoked to partner with Phil and the Run It Once crew on designing the first official Run It Once card deck.</p>
+							</div>
+
+							<div className="deckStack">
 								<img src={img.deck.one} className="deckCard" ref="deckCard01" />
 								<img src={img.deck.two} className="deckCard" ref="deckCard02" />
 								<img src={img.deck.three} className="deckCard" ref="deckCard03" />
 								<img src={img.deck.four} className="deckCard" ref="deckCard04" />
 								<img src={img.deck.five} className="deckCard" ref="deckCard05" />
+								<img src={img.deck.six} className="deckCard" ref="deckCard06" />
 							</div>
 
-							<div className="grid-1-2 grid-copy">
-								<p>Run It Once, created by legendary poker player Phil Galfond, is a place for poker enthusiasts to gather and contribute professional-level strategy with others in the poker community. Besides the wealth of knowledge available at Run It Once, RIO’s brand is one classy act. With a clean, professional, and luxurious logo its no wonder their site is one of the best looking (and functioning) poker communities out there.</p>
-
-								<p>At Underbelly, we’re suckers for playing card designs. That’s one of the many reasons we were stoked to partner with Phil and the Run It Once crew on designing the first official Run It Once card deck.</p>
-
-                        <img src={img.deck.six} className="deckCard" ref="deckCard06" />
-							</div>
 						</div>
 					</section>
 
@@ -149,15 +150,17 @@ class RunItOnce extends Component {
 					</section>
 
 					<section className="case-study-cards" ref="rotateCards">
-						<div className="grid-1-2 grid-copy">
-							<p>Matching the palatial look and feel of Run It Once’s brand was no small feat. We took multiple approaches before finally landing on a style that was sleek, geometric, and modern. Each suit was designed with a unique personality to give the deck depth and variety while still remaining true to RIO’s brand. Diamonds crafted to be rugged and adventurous, spades strong and ruthless, clubs secretive and seductive, and hearts trustworthy and approachable.</p>
+						<div className="container-sm">
+							<div className="rText grid-copy">
+								<p>Matching the palatial look and feel of Run It Once’s brand was no small feat. We took multiple approaches before finally landing on a style that was sleek, geometric, and modern. Each suit was designed with a unique personality to give the deck depth and variety while still remaining true to RIO’s brand. Diamonds crafted to be rugged and adventurous, spades strong and ruthless, clubs secretive and seductive, and hearts trustworthy and approachable.</p>
 
-							<p>The finished product is a world-class, unique deck of cards worthy of the most talented professional poker hands. However, no need to worry; you don’t have to have a bracelet under your belt to enjoy these cards. Anyone can purchase these beauties directly from RIO’s site- even if you’re one of those casual, low-stakes, hold-em folks. If you’re anything like us, you can’t pass up a hot deck of cards.</p>
-						</div>
-						<div className="grid-1-2">
-							<img src={img.cards.one} className="card" ref="rCard01" alt=""/>
-							<img src={img.cards.two} className="card" ref="rCard02" alt=""/>
-							<img src={img.cards.three} className="card" ref="rCard03" alt=""/>
+								<p>The finished product is a world-class, unique deck of cards worthy of the most talented professional poker hands. However, no need to worry; you don’t have to have a bracelet under your belt to enjoy these cards. Anyone can purchase these beauties directly from RIO’s site- even if you’re one of those casual, low-stakes, hold-em folks. If you’re anything like us, you can’t pass up a hot deck of cards.</p>
+							</div>
+							<div className="rCards">
+								<img src={img.cards.one} className="card" ref="rCard01" alt=""/>
+								<img src={img.cards.two} className="card" ref="rCard02" alt=""/>
+								<img src={img.cards.three} className="card" ref="rCard03" alt=""/>
+							</div>
 						</div>
 					</section>
 
