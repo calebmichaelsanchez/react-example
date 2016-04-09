ef6a9a0814092f018f71fee312e001db6d3eb64a
Anthony Calzadilla (Work MBP) <anthony.calzadilla@nextu.com>
2016-04-09 00:49:19 -0400

    wrapped up styles and lay for splay page

--- app/screens/App/screens/Work/screens/SizzlingPlatter/components/SizzlingPlatter.js
+++ app/screens/App/screens/Work/screens/SizzlingPlatter/components/SizzlingPlatter.js
@@ -177,22 +177,29 @@ class SizzlingPlatter extends Component {
 			},
 			patterns: {
 				swatches		: require("../../../../../../../images/work/sizzling-platter/patterns/swatches.png"),
-				peppers		: require("../../../../../../../images/work/sizzling-platter/patterns/peppers.png"),
+				peppers			: require("../../../../../../../images/work/sizzling-platter/patterns/peppers.png"),
 				pepperonis	: require("../../../../../../../images/work/sizzling-platter/patterns/pepperonis.png"),
-				sprinkles	: require("../../../../../../../images/work/sizzling-platter/patterns/sprinkles.png")
+				sprinkles		: require("../../../../../../../images/work/sizzling-platter/patterns/sprinkles.png")
 			},
 			ingredients: {
 				bg		: require("../../../../../../../images/work/sizzling-platter/ingredients/bg.png"),
-				one	: require("../../../../../../../images/work/sizzling-platter/ingredients/01.png"),
-				two	: require("../../../../../../../images/work/sizzling-platter/ingredients/02.png"),
+				crsl	: require("../../../../../../../images/work/sizzling-platter/ingredients/carousel.png"),
+				one		: require("../../../../../../../images/work/sizzling-platter/ingredients/01.png"),
+				two		: require("../../../../../../../images/work/sizzling-platter/ingredients/02.png"),
 				three : require("../../../../../../../images/work/sizzling-platter/ingredients/03.png"),
 				four  : require("../../../../../../../images/work/sizzling-platter/ingredients/04.png")
 			},
 			designDev : {
 				bgLeft  : require("../../../../../../../images/work/sizzling-platter/design-dev/design-dev-bg-left.png"),
 				bgRight : require("../../../../../../../images/work/sizzling-platter/design-dev/design-dev-bg-right.png"),
-				iphone  : require("../../../../../../../images/work/sizzling-platter/design-dev/design-dev-iphone.png"),
-				laptop  : require("../../../../../../../images/work/sizzling-platter/design-dev/design-dev-laptop.png")
+				iphone  : require("../../../../../../../images/work/sizzling-platter/design-dev/iphone.png"),
+				iphoneBgLeft  : require("../../../../../../../images/work/sizzling-platter/design-dev/iphone-bg-left.png"),
+				iphoneBgRight  : require("../../../../../../../images/work/sizzling-platter/design-dev/iphone-bg-right.png"),
+				squarespace  : require("../../../../../../../images/work/sizzling-platter/design-dev/squarespace.png"),
+				redClose  : require("../../../../../../../images/work/sizzling-platter/design-dev/red-close.png"),
+				greyClose  : require("../../../../../../../images/work/sizzling-platter/design-dev/grey-close.png"),
+				greyFar  : require("../../../../../../../images/work/sizzling-platter/design-dev/grey-far.png"),
+				laptop  : require("../../../../../../../images/work/sizzling-platter/design-dev/laptop.png")
 			},
 			lifestyle : require("../../../../../../../images/work/fluid/lifestyle/01.jpg"),
 			seeMore: {
@@ -245,33 +252,45 @@ class SizzlingPlatter extends Component {
 								<h1>THE INGREDIENTS</h1>
 								<p>The Underbelly team loves to incorporate custom photography whenever possible. We knew from day one we’d want to embark on a photoshoot for this project. We wanted crisp, clean photos taken at actual SPAT locations around the valley, not boring stock photos. Not only did get great shots on location, but we held an in-house photoshoot to get shots of ingredients like pepperoni, mushrooms, and even sprinkles! Pulling together high quality, custom photos allowed the best assets possible for our client and the site…nevermind the fact that it left us craving pizza and donuts every time we worked on the site.</p>
 							</div>
-							<div className="ingredients-container__item ingredients-gallery">
-								<div className="ingredients-gallery__item grid-1-4"><img src={img.ingredients.one} /></div>
-								<div className="ingredients-gallery__item grid-1-4"><img src={img.ingredients.two} /></div>
-								<div className="ingredients-gallery__item grid-1-4"><img src={img.ingredients.three} /></div>
-								<div className="ingredients-gallery__item grid-1-4"><img src={img.ingredients.four} /></div>
-							</div>
 						</div>
 					</div>
 
+					<div className="ingredients-hero"></div>
+
+					<div className="ingredients-gallery">
+						<img src={img.ingredients.crsl} />
+					</div>
+
 					<div className="design--dev">
-						<div className="design--dev-container">
-							<div className="design--dev-container__item copy-01">
+							<div className="design--dev__item copy-01">
 								<h1>MADE TO ORDER</h1>
 								<p>It was important to make this revamped site inviting for both investors and potential employees. SPLAT is poud of the cultural diversity within their family. To celebrate this, we wanted to make the site inviting for all audiences. Throughout the site we showcase Sizzling Platter’s investment in their ever expanding SPLAT family, diversity, and strong company values.</p>
-								<img src={img.ingredients.one} />
 							</div>
 
-							<div className="design--dev-container__item copy-02">
-								<h1>CUSTOM EXPERIENCE<br /> VIA SQUARESPACE</h1>
+							<div className="design--dev__item iphone">
+								<img className="iphone__item" src={img.designDev.iphone} />
+								<img className="iphone__item" src={img.designDev.iphoneBgLeft} />
+								<img className="iphone__item" src={img.designDev.iphoneBgRight} />
+							</div>
+
+							<img className="ss" src={img.designDev.squarespace} />
+
+							<div className="design--dev__item squarespace">
+								<h1>CUSTOM EXPERIENCE <br />VIA SQUARESPACE</h1>
 								<p>We chose Squarespace for our content management system to allow easy modification with a great custom design. Although the Squarespace Development Platform is still in its infancy, the development team was able to pour though documentation to create a beautiful website that combines Squarespace’s wonderful backend with React based front-end. Using React in conjunction with Axios we were able to leverage the way Squarespace exposes site data to create clean, reusable components.</p>
+									<img className="squarespace__item" src={img.designDev.redClose} />
+									<img className="squarespace__item" src={img.designDev.greyClose} />
+									<img className="squarespace__item" src={img.designDev.greyFar} />
+						</div>
+
+							<div className="design--dev__item laptop">
+								<img src={img.designDev.laptop} />
 							</div>
 
-							<div className="design--dev-container__item copy-03">
+							<div className="design--dev__item copy-03">
 								<h1>SERVING THE PEOPLE</h1>
 								<p>One of our primary goals for the Sizzling Platter redesign was to highlight their focus on company culture. Sizzling Platter strives to uphold a diverse culture and strong core values from the highest level of stakeholder to potential employees. By tying in human elements throughout the site, we hope the SPLAT culture resonates with visitors as much as it did with our team!</p>
 							</div>
-						</div>
 					</div>
 
 
