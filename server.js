50af3b49c3f85d1ffeac20710ed690ce6b3a3b6b
Caleb Sanchez <calebmichaelsanchez@gmail.com>
2016-06-13 16:28:48 -0600

    Server testing

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', function(req, res) {
-  res.sendFile('index.html', { root: __dirname + '/public/') });
+  res.sendFile('index.html', { root: __dirname + '/public/' });
 });
 
 app.get('*', (req, res) => {
